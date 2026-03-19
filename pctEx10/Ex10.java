d49116be5927540e98de4d8e21e99e4555566862
bessaisabela56-boop <bessaisabela56@gmail.com>
2026-03-19 11:03:44 -0300

    Add files via upload

---
+++ pctEx10/Ex10.java
@@ -0,0 +1,12 @@
+package pctEx10;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		double b = 6 , h = 18;
+		
+		System.out.println("\n Calculo do Perimetro de Retangulo: ");
+		System.out.println("\n "+ b + " + " + b+ " + "+ h+" + "+ h+" = "+ (b+b+h+h) );
+	}
+
+}
